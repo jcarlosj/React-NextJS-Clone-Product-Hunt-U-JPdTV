6595597786d18a257f3694ebb2ecd362e44a3dc9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-11 15:46:59 -0500

    [+] Next > Routing: Create navigation links

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const MainLayout = props => {
     return( 
         <>
             <h1>Header</h1>
+            <nav>
+                <Link href="/">Inicio</Link>
+                <Link href="/us">Nosotros</Link>
+            </nav>
             <main>
                 { props .children }
             </main> 

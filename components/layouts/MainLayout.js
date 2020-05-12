272175fb7fcb18dcaff0b94373b06e4a36df513e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-12 13:20:01 -0500

    [+] Styles: Implements static CSS, Google Fonts, Normalize

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import { Global, css } from '@emotion/core';
 
 /** Components */
@@ -25,6 +26,7 @@ const MainLayout = props => {
                     } 
 
                     body {
+                        font-family: 'PT Sans', sans-serif;
                         font-size: 1.6rem;
                         line-height: 1.5;
                     }
@@ -32,6 +34,13 @@ const MainLayout = props => {
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     ul {
@@ -46,6 +55,13 @@ const MainLayout = props => {
 
                 `}
             />
+            <Head>
+                <html lang="es" />
+                <title>Product Hunt - Firebase & Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
+                <link href="./static/css/master.css" rel="stylesheet" />
+            </Head>
             <Header />
             
             <main>

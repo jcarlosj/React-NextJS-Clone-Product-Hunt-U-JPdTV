c41604f8c9d85e107eb93ddcf39f3eea9d40695d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-12 11:29:28 -0500

    [+] Styles > emotion: Implement global style sheets

--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
+import { Global, css } from '@emotion/core';
 
 /** Components */
 import Header from './Header';
@@ -7,6 +7,45 @@ import Header from './Header';
 const MainLayout = props => {
     return( 
         <>
+            <Global 
+                styles={ css `
+                    :root {
+                        --gray-one: #3D3D3D;
+                        --gray-two: #6F6F6F;
+                        --orange: #DA552F;
+                    }
+
+                    html {
+                        font-size: 62.5%;
+                        box-sizing: border-box;
+                    }
+
+                    *, *:before, *:after {
+                        box-sizing: inherit;
+                    } 
+
+                    body {
+                        font-size: 1.6rem;
+                        line-height: 1.5;
+                    }
+
+                    h1, h2, h3 {
+                        margin: 0 0 2rem 0;
+                        line-height: 1.5;
+                    }
+
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+
+                    a {
+                        text-decoration: none;
+                    }
+
+                `}
+            />
             <Header />
             
             <main>

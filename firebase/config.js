ea73202bab8cc36ff189f1fde370976bd793759b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-14 09:56:11 -0500

    [+] NPM > firebase, firebase-admin: Install dependency and add firebase configuration

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDI-qKZe2ecL0rwmVdEW-_Go80Z0vR1Pqw",
+    authDomain: "product-hunt-41090.firebaseapp.com",
+    databaseURL: "https://product-hunt-41090.firebaseio.com",
+    projectId: "product-hunt-41090",
+    storageBucket: "product-hunt-41090.appspot.com",
+    messagingSenderId: "986402401461",
+    appId: "1:986402401461:web:5fa9227bf4bbcd2c4c22fc"
+};
+
+export default firebaseConfig;

1bf1cbfb4cd6dc725a0f38e36ee648b872002a02
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-14 16:37:41 -0500

    [+] Component > Navigation(Context): Conditions display menu item 'new product'

--- components/layouts/Navigation.js
+++ components/layouts/Navigation.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+/** Dependencies */
 import Link from 'next/link';
 import styled from '@emotion/styled';
+
+/** Context */
+import { FirebaseContext } from '../../firebase';
 
 /** Define Style Component */
 const Nav = styled .nav `
@@ -20,11 +25,15 @@ const Nav = styled .nav `
 
 /** Define Component */
 const Navigation = () => {
+
+    const /** Destructuring properties 'FirebaseContext' */
+        { user } = useContext( FirebaseContext );
+
     return( 
         <Nav>
             <Link href="/"><a>Inicio</a></Link>
             <Link href="/popular"><a>Populares</a></Link>
-            <Link href="/new-product"><a>Nuevo Producto</a></Link>
+            { user && <Link href="/new-product"><a>Nuevo Producto</a></Link> }
         </Nav>
     );
 }

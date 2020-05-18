6a609a53439196b9273402b878273b80db0e944d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-18 16:43:38 -0500

    [+] Component > Product - [id].js: Get value of Product ID parameter using Next router

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 /** Dynamic Component 
  * Construirá un Componente para cada ruta dinámica
 */
 const Product = () => {
 
+    const   /** Obtener el parámetro pasado por la URL */
+        router = useRouter(),
+        { query: { id } } = router;     // Destructuring para obtener el ID del producto
+
+    console .log( 'ID', id );
+
+    /** Tracking 'id' */
+    useEffect( () => {
+        /** Valida que el ID exista */
+        if( id ) {
+            console .log( `Ya hay un ID` );
+        }
+    }, [ id ] );
+
     return (
-        <h1>Archivo [id].js</h1>
+        <>
+            <h1>Archivo [id].js</h1>
+            <p>ID: { id }</p>
+        </>
     );
 }
 

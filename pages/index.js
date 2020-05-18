7c9494c9e5834c27aa25e9488f9a192b7b1432fe
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-18 10:25:12 -0500

    [+] Page > index: Consult Firebase database to obtain products ordered by creation date

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
+import React, { useState, useEffect, useContext } from 'react';
 import styled from '@emotion/styled';                       // Dependency
 import MainLayout from '../components/layouts/MainLayout';  // Component
-
-/** Define Style Components */
-const Heading = styled .h1 `
-    background-color: green;
-    color: white;
-    margin: 0;
-    padding: .5rem 1rem;
-    text-align: center;
-`;
+import { FirebaseContext } from '../firebase';
 
 export default function Home() {
+
+    const
+        [ products, setProducts ] = useState( [] ),     // Define State 'products'
+        { firebase } = useContext( FirebaseContext );   // Extract Context
+
+    /** Traking */
+    useEffect( () => {
+        /** Consultar API de Firebase cuando el componente cargue */
+        const getProducts = () => {
+            firebase .db .collection( 'products' ) 
+                         .orderBy( 'creationDate', 'desc' )
+                         .onSnapshot( handleSnapShot );         // Permite acceder a los datos consultados
+        }
+        getProducts();
+    }, [] );
+
+    /** Manejador de Datos Consultados en FireBase */
+    const handleSnapShot = snapshot  => {
+        const products = snapshot .docs .map( document => {
+            return {
+                id: document .id,
+                ...document .data()      // Get data
+            }
+        });
+
+        console .log( 'Products', products );
+        setProducts( products );        // Update State 
+    }
+
     return (
         <MainLayout>
-            <Heading>
-                Inicio
-            </Heading>
+            <h1>Inicio</h1>
         </MainLayout>
     )
 }

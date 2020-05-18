57e1422b03f335fc923508c0cb5fbc13774f19d3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-18 11:30:14 -0500

    [+] Component > ProductDetail: Display product data

--- components/layouts/ProductDetail.js
+++ components/layouts/ProductDetail.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
+/** Dependencies */
+import styled from '@emotion/styled';
+
+/** Define Style Component */
+const Image = styled .img `
+    width: 200px;
+`;
+
 const ProductDetail = ({ product }) => {
 
     const
         { id, name, companyName, productName, productUrl, productImageUrl, productDescription, comments, votes, creationDate } = product;
 
     return (
-        <li>{ productName }</li>
+        <li>
+            <section>
+                <section>
+                    <Image src={ productImageUrl } />
+                </section>
+            </section>
+            <section>
+                <h2>{ productName }</h2>
+                <p>{ productDescription }</p>
+                <section>
+                    <img src="./static/images/comment.png" />
+                    <p>{ comments .lenght } Comentarios</p>
+                </section>
+            </section>
+            <section>
+                <div> &#9650; </div>
+                <p>{ votes }</p>
+            </section>
+        </li>
     );
 }
 

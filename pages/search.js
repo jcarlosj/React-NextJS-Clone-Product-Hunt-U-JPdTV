ee847f3903ef413bfa1e65ecb445d8a03e0bfa9d
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-05-22 13:48:48 -0500

    [+] Page > search: Search filtering results by fields name and product description

--- pages/search.js
+++ pages/search.js
@@ -1,29 +1,67 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';  
+import { useRouter } from 'next/router';                
 
-import styled from '@emotion/styled';                       // Dependency
-import MainLayout from '../components/layouts/MainLayout';  // Component
+/** Dependencies */
+import { css } from '@emotion/core';
 
-/** Define Style Components */
-const Heading = styled .h1 `
-    background-color: orange;
-    color: white;
-    margin: 0;
-    padding: .5rem 1rem;
-    text-align: center;
-`;
+/** Components  */
+import MainLayout from '../components/layouts/MainLayout';
+import ProductDetail from '../components/layouts/ProductDetail';
+
+/** Hooks */
+import useProducts from '../hooks/useProducts';
 
 /** Component */
 const Search = () => {
 
-    const router = useRouter();
-    console .log( 'Router', router );
+    const 
+        /** Define State 'results' */
+        [ resultsObtained, setResultsObtained ] = useState([]),
+        /** Obtine parametros pasados al componente por la URL */
+        router = useRouter(),
+        { query: { q } } = router,
+        /** Destructuring hook data 'products' */
+        { products } = useProducts( 'creationDate' );    // Establece la propiedad que establecerá el ordenamiento de nuestra lista de productos
+
+    console .log( 'Termino de busqueda', q );
+    
+    /** Tracking 'q' & 'products' */
+    useEffect( () => {
+        const 
+            searchTerm = q .toLowerCase(),
+            productsFound = products .filter( product => {
+                return (
+                    product .productName .toLowerCase() .includes( searchTerm )
+                    || product .productDescription .toLowerCase() .includes( searchTerm )
+                );
+            });
+
+        console .log( 'Productos Encontrados', productsFound );
+
+        setResultsObtained( productsFound );    // Update State 
+
+    }, [ q, products ] );
 
     return (
         <MainLayout>
-            <Heading>
-                Buscar
-            </Heading>
+            <div className="product-list">
+                <div className="container">
+                    <ul className="bg-white">
+                        { ( resultsObtained .length === 0 )
+                            ?   <h3 css={ css `
+                                    padding: 3rem 0;
+                                    text-align: center;
+                                ` }>No se encontro productos con resultados cohincidentes</h3>
+                            :   resultsObtained .map( product => (
+                                    <ProductDetail
+                                        key={ product .id }
+                                        product={ product }
+                                    />
+                                ))
+                        }
+                    </ul>
+                </div>
+            </div>
         </MainLayout>
     )
 }
